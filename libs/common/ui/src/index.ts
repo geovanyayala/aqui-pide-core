a8a9d50e2863c84a4fac61678ac778f5b3d6e29b
geovanyayala <geovanyayala@gmail.com>
2023-12-21 22:12:28 -0700

    Add code for angular-17.0.0 side-nav.
    [Url: razroo.com/community/angular-17.0.0/side-nav-recipe/side-nav]

--- libs/common/ui/src/index.ts
+++ libs/common/ui/src/index.ts
@@ -5,3 +5,4 @@ export * from "./lib/global-footer/global-footer.component";
 export * from "./lib/global-sidenav/global-sidenav.component";
 export * from "./lib/global-header/global-header.component";
 export * from "./lib/global-footer/global-footer.component";
+export * from "./lib/global-sidenav/global-sidenav.component";
