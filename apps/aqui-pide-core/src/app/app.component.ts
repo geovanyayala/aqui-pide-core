984c9a241f8f1a75b63bb41b5c9b36602dccf85e
geovanyayala <geovanyayala@gmail.com>
2023-12-21 21:58:26 -0700

    Add code for angular-17.0.0 create-workspace.
    [Url: razroo.com/community/angular-17.0.0/create-workspace-recipe/create-workspace]

---
+++ apps/aqui-pide-core/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aqui-pide-core-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'aqui-pide-core';
+}
