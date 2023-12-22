b8a8571c3ce09fd5a2d82d77bf9a33c3f0d9ecdf
geovanyayala <geovanyayala@gmail.com>
2023-12-21 22:12:09 -0700

    Add code for angular-17.0.0 integrate-side-nav.
    [Url: razroo.com/community/angular-17.0.0/side-nav-recipe/integrate-side-nav]

--- apps/aqui-pide-core/src/app/app.module.ts
+++ apps/aqui-pide-core/src/app/app.module.ts
@@ -18,10 +18,12 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./routing/app-routing.module";
 import { GlobalHeaderComponentModule } from "@aqui-pide-core/common/ui";
 import { GlobalFooterModule } from "@aqui-pide-core/common/ui";
+import { GlobalSidenavModule } from "@aqui-pide-core/common/ui";
+import { MatListModule } from "@angular/material/list";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, BrowserAnimationsModule, GlobalSidenavModule, MatListModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, BrowserAnimationsModule, GlobalSidenavModule, MatListModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
