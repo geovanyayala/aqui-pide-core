496948813d7ff4a6f98395614a9b5f116ad24b44
geovanyayala <geovanyayala@gmail.com>
2023-12-21 22:11:08 -0700

    Add code for angular-17.0.0 add-header-toggle-event.
    [Url: razroo.com/community/angular-17.0.0/side-nav-recipe/add-header-toggle-event]

--- apps/aqui-pide-core/src/app/app.component.ts
+++ apps/aqui-pide-core/src/app/app.component.ts
@@ -7,7 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   sideNavOpen = false;
+  sideNavOpen = false;
   title = 'aqui-pide-core';
+
+  sideNavToggle(): void {
+    this.sideNavOpen = !this.sideNavOpen;
+  }
 
   sideNavToggle(): void {
     this.sideNavOpen = !this.sideNavOpen;
