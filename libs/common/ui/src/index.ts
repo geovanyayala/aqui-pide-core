b58ac0b7fe576d553dbd570812145410bf7600af
geovanyayala <geovanyayala@gmail.com>
2023-12-21 22:11:12 -0700

    Add code for angular-17.0.0 create-common-ui-lib.
    [Url: razroo.com/community/angular-17.0.0/create-common-ui-lib/create-common-ui-lib]

--- libs/common/ui/src/index.ts
+++ libs/common/ui/src/index.ts
@@ -1,4 +1,1 @@
 export * from './lib/ui.module';
-export * from "./lib/layout/layout.component";
-export * from "./lib/global-header/global-header.component";
-export * from "./lib/global-sidenav/global-sidenav.component";
