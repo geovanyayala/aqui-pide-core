6d5c018dae5baf353d59fd20f05f3f7de057a7fb
geovanyayala <geovanyayala@gmail.com>
2023-12-21 22:10:52 -0700

    Add code for angular-17.0.0 integrate-ngrx-store.
    [Url: razroo.com/community/angular-17.0.0/add-ngrx-store-infrastructure/integrate-ngrx-store]

--- apps/aqui-pide-core/src/app/app.module.ts
+++ apps/aqui-pide-core/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppComponent } from './app.component';
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { StoreModule } from "@ngrx/store";
 import { environment } from "@aqui-pide-core/common/common-environment";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { StoreModule } from "@ngrx/store";
+import { environment } from "@aqui-pide-core/common/common-environment";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, StoreModule.forRoot({}), StoreDevtoolsModule.instrument({
+    maxAge: 25, // Retains last 25 states
+    logOnly: environment.production, // Restrict extension to log-only mode
+    autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+  }), StoreModule.forRoot({}), StoreDevtoolsModule.instrument({
     maxAge: 25, // Retains last 25 states
     logOnly: environment.production, // Restrict extension to log-only mode
     autoPause: true, // Pauses recording actions and state changes when the extension window is not open
